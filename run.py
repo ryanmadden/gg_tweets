c8045bfa7ca8472e03e20225678ee845bf6effd5
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-02-08 16:12:04 -0600

    general algorithm good

--- run.py
+++ run.py
@@ -1,44 +1,65 @@
 import time
-import timeit
-import nltk
-from nltk import word_tokenize
 import json
-import preprocess
+from nltk import word_tokenize
 
 
-"""
-Timing decorator to monitor a function's duration
-"""
 def timeit(f):
-
     def timed(*args, **kw):
         start     = time.time()
         result    = f(*args, **kw)
         end       = time.time()
-
         print 'func:%r took: %2.4f sec' % (f.__name__, end-start)
         return result
 
     return timed
 
 
+
+def find_names(tweet):
+	pass
+
+
+
+def determine_results(hosts, presenters, nominees):
+	pass
+
+
+
 @timeit
 def main():
-	filename_2015_mini = './gg15mini.json'
-	filename_2015      = './goldenglobes2015.json'
-	filename_2013      = './gg2013.json'
-	count = 0
-	wordset = preprocess.create_preprocess_wordset()
+	f_2015_mini = './gg15mini.json'
+	f_2015      = './goldenglobes2015.json'
+	f_2013      = './gg2013.json'
 
-	with open(filename_2013, 'r') as f:
-		loaded = json.loads(f)
-		for d in loaded:
-			for key, value in d.iteritems():
-				if key == 'text':
-					lower_text  = value.lower()
-					if preprocess.filter_tweet(lower_text, wordset):
-						count +=1
-	print count
+	host_filters      = ["host", "hosting", "hosts", "hosted"]
+	presenter_filters = ["presented", "presenting", "presenter", "presenters"]
+	award_names = [] #TODO
+
+	hosts      = []
+	presenters = {}
+	nominees   = {}
+
+	with open(f_2013, 'r') as f:
+		tweets = map(json.loads, f)[0]
+
+		for tweet in tweets:
+			text  = tweet['text']
+			names = ""
+
+			if filt in text for filt in host_filters:
+				names = find_names(text)
+				hosts.extend(names)
+
+			if award in text for award in award_names:
+				if not names:
+					names = find_names(text)
+				nominees[award].extend(names)
+
+				if filt in text for filt in presenter_filters:
+					presenters[award].extend(names)
+
+
+
 
 
 if __name__ == "__main__":
