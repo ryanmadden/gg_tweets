487c2bf2b4f57bfa6e74208dfde131d60d2248ec
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2015-02-08 13:57:55 -0600

    code cleaned

--- preprocess.py
+++ preprocess.py
@@ -1,58 +1,26 @@
 import time
 from nltk import word_tokenize
 import json
-"""
-Timing decorator to track how long a function takes
-@timeit
-"""
+
+
 def timeit(f):
-
     def timed(*args, **kw):
-
-        ts = time.time()
+        start  = time.time()
         result = f(*args, **kw)
-        te = time.time()
-
-        print 'func:%r took: %2.4f sec' % \
-          (f.__name__, te-ts)
+        end    = time.time()
+        print 'func:%r took: %2.4f sec' % (f.__name__, te-ts)
         return result
-
     return timed
 
-# preprocess tweets module 
-#@timeit
+
 def filter_tweet(tweet, wordset):
-	#O(1) lookup instead of O(n) for a list
-	#Much faster
 	if any([True for word in word_tokenize(tweet) if word in wordset]):
 		return True
-	else:
-		return False
+	return False
+
 
 def create_preprocess_wordset():
 	wordlist = []
 	with open('./preprocess.txt', 'r') as f:
 		wordlist = f.read().replace('\n','').split(',')
-		return set(wordlist) 
-# @timeit
-# def main():
-# 	origin_filename = './goldenglobes2015.json'
-# 	new_filename = './processed.json'
-# 	count = 0
-# 	wordset = create_preprocess_wordset()
-
-# 	n = open(new_filename, 'w')
-# 	n.truncate()
-# 	with open(origin_filename, 'r') as f:
-# 		for line in f:
-# 			if count > 1000000:
-# 				break
-# 			text = json.loads(line)[0]['text']
-# 			lower_text = text.lower()
-# 			if filter_tweet(lower_text, wordset):
-# 				n.write(line)
-# 			count+=1
-		
-
-# if __name__ == "__main__":
-# 	main()
+		return set(wordlist)
--- run.py
+++ run.py
@@ -1,10 +1,3 @@
-# data = []
-# with open('gg2013.json') as f:
-#     for line in f:
-#         data.append(json.loads(line))
-#         print line
-#         break
-
 import time
 import timeit
 import nltk
@@ -31,48 +24,20 @@ def timeit(f):
 
 @timeit
 def main():
-	count = 0
-
 	filename_2015_mini = './gg15mini.json'
 	filename_2015      = './goldenglobes2015.json'
 	filename_2013      = './gg2013.json'
+	count = 0
 	wordset = preprocess.create_preprocess_wordset()
-	
+
 	with open(filename_2013, 'r') as f:
-		for line in f:
-			loaded = json.loads(line)
-			for d in loaded:
-				for key, value in d.iteritems():
-					if key == 'text':
-						lower_text  = value.lower()
-
-						"""
-							Preprocess and filter out unuseful tweets based on preprocess.txt
-							Only do the expensive people parsing step if the tweet has any of the
-							keywords we identify at the beginning
-							Speedup of around ~5times
-							
-						"""
-						if preprocess.filter_tweet(lower_text, wordset):
-
-							# token_dict = text_to_token_dict(lower_text,STOP_WORDS,True)
-							count +=1
-							# print value
-							# if any([True for tok in token_dict if tok in SOW]):
-							"""This people dict step in NLTK is slow"""
-							#people_dict = text_to_people_dict(text)
-							# people_dict =  text_to_people_dict_naive_fast(text,FOW)
-
-							# memorize_people_if_tokens_match(token_dict, people_dict, MEMORY, IMPORTANT_WORDS)
-
-							# _trie_memorize_people_if_tokens_match(token_dict, people_dict, TRIE, IMPORTANT_WORDS)
-
-	#pprint(MEMORY)
-	#pprint(TRIE)
-	# get_top_n_vals(MEMORY, 5)
-
-	# trie_top_n_vals(TRIE, 5)
-	# pprint(get_trie_top_n_vals(TRIE, 5))
+		loaded = json.loads(f)
+		for d in loaded:
+			for key, value in d.iteritems():
+				if key == 'text':
+					lower_text  = value.lower()
+					if preprocess.filter_tweet(lower_text, wordset):
+						count +=1
 	print count
 
 
