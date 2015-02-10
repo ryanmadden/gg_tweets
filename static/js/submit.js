d87238aeee3827a9663113aeeda45a8cac48ce88
grant nicholas <grantnicholas2015@u.northwestern.edu>
2015-02-09 21:45:45 -0600

    front-end changes

--- static/js/submit.js
+++ static/js/submit.js
@@ -20,3 +20,25 @@ $(document).ready(function($){
 
 
 });
+
+
+$(function() {
+
+    var $sidebar   = $("#sidebar"), 
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        topPadding = 15;
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 0
+            });
+        }
+    });
+    
+});
