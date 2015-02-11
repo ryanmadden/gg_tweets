c39cc7aa669b09e64522b32be369321703cd9cbb
senluchen2015 <senluchen2015@u.northwestern.edu>
2015-02-10 21:34:26 -0600

    change award names and fix frontend list

--- static/js/submit.js
+++ static/js/submit.js
@@ -22,23 +22,23 @@ $(document).ready(function($){
 });
 
 
-$(function() {
+// $(function() {
 
-    var $sidebar   = $("#sidebar"), 
-        $window    = $(window),
-        offset     = $sidebar.offset(),
-        topPadding = 15;
+//     var $sidebar   = $("#sidebar"), 
+//         $window    = $(window),
+//         offset     = $sidebar.offset(),
+//         topPadding = 15;
 
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.top) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding
-            });
-        } else {
-            $sidebar.stop().animate({
-                marginTop: 0
-            });
-        }
-    });
+//     $window.scroll(function() {
+//         if ($window.scrollTop() > offset.top) {
+//             $sidebar.stop().animate({
+//                 marginTop: $window.scrollTop() - offset.top + topPadding
+//             });
+//         } else {
+//             $sidebar.stop().animate({
+//                 marginTop: 0
+//             });
+//         }
+//     });
     
-});
+// });
