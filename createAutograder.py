87ccb7ae75f79285720d2926dc883e6493f885c5
senluchen2015 <senluchen2015@u.northwestern.edu>
2015-02-18 16:46:06 -0600

    change description format

--- createAutograder.py
+++ createAutograder.py
@@ -14,18 +14,11 @@ def create_meta_data(year):
 	metadata["year"] = year
 	names = {}
 
-	hosts_d      = """
-			   	   Hosts were detected from the file by doing a simple frequency based analysis on tweets mentioning the words "hosts", "hosting", etc. IE) For every tweet mentioning the "hosting" keywords, 
-				   all the names in the tweets were extracted using a regex, these names were put into a dictionary that counts their frequency of occurence, and the top results based on frequency of occurrence were
-				   counted as the hosts. 
+	hosts_d      = """Hosts were detected from the file by doing a simple frequency based analysis on tweets mentioning the words "hosts", "hosting", etc. IE) For every tweet mentioning the "hosting" keywords, all the names in the tweets were extracted using a regex, these names were put into a dictionary that counts their frequency of occurence, and the top results based on frequency of occurrence were counted as the hosts. 
 			       """
-	nominees_d   = """
-				   Nominees were hardcoded from a source available before the goldenglobes were released. This was done in order to improve the detection of winners. IE) Instead of detecting from the entire possible
-				   set of words in the english language who the winner was, we limited our problem to detecting who the winner was given 4 or 5 choices of nominees. This *greatly* reduces the problem scope.
+	nominees_d   = """Nominees were hardcoded from a source available before the goldenglobes were released. This was done in order to improve the detection of winners. IE) Instead of detecting from the entire possible set of words in the english language who the winner was, we limited our problem to detecting who the winner was given 4 or 5 choices of nominees. This *greatly* reduces the problem scope.
 				   """
-	awards_d     = """
-				   The actual award names were hardcoded into the file in order to improve accuracy and because the goldenglobes have consistent award names on a yearly basis. In order to adapt this approach 
-				   to other awards shows the list of awards would have to be scraped and then supplied as input to the program. 
+	awards_d     = """The actual award names were hardcoded into the file in order to improve accuracy and because the goldenglobes have consistent award names on a yearly basis. In order to adapt this approach to other awards shows the list of awards would have to be scraped and then supplied as input to the program. 
 				   """
 	presenters_d = """
 				   A list of presenters name was hardcoded in order to improve mappings 			  
@@ -45,15 +38,9 @@ def create_meta_data(year):
 	metadata["names"] = names
 	mappings = {}
 
-	nominees_mapping_d = """
-						We mapped the nominees to the award by hardcoding it. 
+	nominees_mapping_d = """We mapped the nominees to the award by hardcoding it. 
 						"""
-	presenters_mapping_d  = """
-							Presenters mapping are done by parsing each tweet using regex to find presenter names and assigned 
-							presetners to award based on the ward keywords. The mapped award presenter list is then post-processed
-							based on criterias: 1. No one can present an ward they are nominated for. 2. If a presenter is listed for 
-							many awards, remove them from awards where they have very few votes compared to their max votes. 
-							3. For each ward, remove presenters who have < 50 percent of the votes of the max			
+	presenters_mapping_d  = """	Presenters mapping are done by parsing each tweet using regex to find presenter names and assigned presetners to award based on the ward keywords. The mapped award presenter list is then post-processed based on criterias: 1. No one can present an ward they are nominated for. 2. If a presenter is listed for many awards, remove them from awards where they have very few votes compared to their max votes. 3. For each ward, remove presenters who have < 50 percent of the votes of the max			
 							"""
 	mappings["nominees"] = create_meta_each("hardcoded", nominees_mapping_d)
 	mappings["presenters"] = create_meta_each("detected", presenters_mapping_d)
